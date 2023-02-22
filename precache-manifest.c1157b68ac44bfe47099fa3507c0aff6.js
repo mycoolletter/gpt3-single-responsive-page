d3d9b1fbc25be34f59f502bdd748ba7226323162
mycoolletters <mycoolletters@gmail.com>
2023-02-22 22:59:05 +0200

    Updates

---
+++ precache-manifest.c1157b68ac44bfe47099fa3507c0aff6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "f454cb4d2321609d6358",
+    "url": "/gpt3-single-responsive-page/static/css/main.b1265b40.chunk.css"
+  },
+  {
+    "revision": "f454cb4d2321609d6358",
+    "url": "/gpt3-single-responsive-page/static/js/main.f454cb4d.chunk.js"
+  },
+  {
+    "revision": "829bfd6551a90206f69f",
+    "url": "/gpt3-single-responsive-page/static/js/1.829bfd65.chunk.js"
+  },
+  {
+    "revision": "b099dd37c6b3cce1fe05",
+    "url": "/gpt3-single-responsive-page/static/js/runtime~main.b099dd37.js"
+  },
+  {
+    "revision": "e019e2253efdf8a58f4b99edc39e0e8e",
+    "url": "/gpt3-single-responsive-page/static/media/logo.e019e225.svg"
+  },
+  {
+    "revision": "93e14c12525c2e42afa3fc70144cce0f",
+    "url": "/gpt3-single-responsive-page/static/media/blog1.93e14c12.png"
+  },
+  {
+    "revision": "320677cdbe725b722200b506eb1e1147",
+    "url": "/gpt3-single-responsive-page/static/media/blog2.320677cd.png"
+  },
+  {
+    "revision": "deece5b4e4c877dd1640145674e6aa16",
+    "url": "/gpt3-single-responsive-page/static/media/blog3.deece5b4.png"
+  },
+  {
+    "revision": "c5f38a179a067a27f88c5b36136220a4",
+    "url": "/gpt3-single-responsive-page/static/media/blog4.c5f38a17.png"
+  },
+  {
+    "revision": "ab0c13b7ab8a31f9296d0bdefbb37523",
+    "url": "/gpt3-single-responsive-page/static/media/blog5.ab0c13b7.png"
+  },
+  {
+    "revision": "64f17dfba609d95a937cd6d2fbb9cf22",
+    "url": "/gpt3-single-responsive-page/static/media/google.64f17dfb.svg"
+  },
+  {
+    "revision": "2d90257c9102890b2355986bdf02cd4f",
+    "url": "/gpt3-single-responsive-page/static/media/atlassian.2d90257c.svg"
+  },
+  {
+    "revision": "3def489d366f9c1306c8ddf72a7913a7",
+    "url": "/gpt3-single-responsive-page/static/media/dropbox.3def489d.svg"
+  },
+  {
+    "revision": "df0e8d5428fda2b7b359c9d7de021bed",
+    "url": "/gpt3-single-responsive-page/static/media/shopify.df0e8d54.svg"
+  },
+  {
+    "revision": "7c9471f364f850f47d03b16b7a14a9bc",
+    "url": "/gpt3-single-responsive-page/static/media/slack.7c9471f3.svg"
+  },
+  {
+    "revision": "cfeb741d0ec8da4bf94f8d77a3c2ce13",
+    "url": "/gpt3-single-responsive-page/static/media/people.cfeb741d.png"
+  },
+  {
+    "revision": "4bbdf2cc0c11bb7c9cb1bd63da5f2f12",
+    "url": "/gpt3-single-responsive-page/static/media/ai.4bbdf2cc.png"
+  },
+  {
+    "revision": "a72811903bd7796b5d81e1a73d9d5a61",
+    "url": "/gpt3-single-responsive-page/static/media/possibility.a7281190.png"
+  },
+  {
+    "revision": "fafdb152b86f0c434ab2b363fa7eef24",
+    "url": "/gpt3-single-responsive-page/index.html"
+  }
+];
